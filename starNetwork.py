1dd78ab6e8c47f1aac99f075a98514a52ba9fb2d
MauRomano <52513848+MauRomano@users.noreply.github.com>
2024-04-10 17:57:39 -0300

    Update starNetwork.py

--- starNetwork.py
+++ starNetwork.py
@@ -21,9 +21,9 @@ import logging
 import simpy
 
 RANDOM_SEED = 42
-NUN_NODES = 5
+NUN_NODES = 20
 
-SIM_TIME = 2000
+SIM_TIME = 20000
 
 random.seed(RANDOM_SEED)
 
@@ -41,25 +41,22 @@ class NodesMgt:
         self.listOfNodes.append(n)
 
     def selectNode(self):
-        a = 1
-        while True:
-            choice = random.choice(self.listOfNodes)
-            if(choice.beingUsed==0):
-                choice.startTransmission()
-                return choice
-
-
+        listNodes = [x for x in self.listOfNodes if x.beingUsed == 0]
+        if (len(listNodes) == 0):
+            return None
+        choice = random.choice(self.listOfNodes)
+        if(choice.beingUsed==0):
+            choice.startTransmission()
+            return choice
 
     def nodeGenerator(self):
         for i in range(self.numNodes):
-            print(i)
             nd = Node(self.env, f"Node_{i}", next(self.type))
             self.appendNodeToList(nd)
 
     def showAllNodes(self):
         for ndName in self.listOfNodes:
             print(ndName)
-            print()
 
 class Coordinator:
     
@@ -71,19 +68,18 @@ class Coordinator:
 
     def answerToNode(self, _node, transmissionTime):
         yield self.env.timeout(transmissionTime)
-        print(f"Answer {_node} at {self.env.now: .2f}")
+        print(f"Coordinator answer to {_node} at {self.env.now: .2f}")
 
 class Node:
     MIN_RATE = 10
-    AVG_RATE = 40
-    MAX_RATE = 50
+    AVG_RATE = 400
+    MAX_RATE = 500
     STD_RATE = 20
 
     MIN_PACK = 200
-    AVG_PACK = 1000
+    AVG_PACK = 2000
     MAX_PACK = 3000
-    STD_PACK = 800
-
+    STD_PACK = 400
 
     def __init__(self, env, name, type) -> None:
         self.name = name
@@ -92,20 +88,27 @@ class Node:
         self.beingUsed = 0
         self.packageSize = self.AVG_PACK
         self.transmissionRate = self.AVG_RATE
+        self.totalTransmitted = 0
+        self.totalTime = 0
+        self.initTime = 0
     
     def defineTransmissionRate(self):
         self.transmissionRate = min(max(self.MIN_RATE, np.random.normal(self.AVG_RATE, self.STD_RATE)), self.MAX_RATE)
     
     def __str__(self) -> str:
+        if (self.totalTime != 0):
+            avgTransmitted = self.totalTransmitted/self.totalTime
+        else: 
+            avgTransmitted = 0
         return f"""
-                Nodename: {self.name};
-                Node type: {self.type};
-                Current Transmission Rate: {self.transmissionRate};
-                Current Package Size: {self.packageSize};
-                Total Data Transmitted: -;
-                Avarage Transmission Rate: -;
-            \n
-            """
+    Nodename: {self.name};
+    Node type: {self.type};
+    Current Transmission Rate: {self.transmissionRate};
+    Current Package Size: {self.packageSize};
+    Total Data Transmitted: {self.totalTransmitted};
+    Total Time Transmitted: {self.totalTime};
+    Average Rate: {avgTransmitted}
+    """
     
     def generatePackageSize(self):
         self.packageSize = min(max(self.MIN_PACK, np.random.normal(self.AVG_PACK, self.STD_PACK)), self.MAX_PACK)
@@ -115,50 +118,36 @@ class Node:
         self.beingUsed = 1
     
     def endTransmission(self):
+        self.totalTransmitted = self.totalTransmitted + self.packageSize
+        self.totalTime = self.totalTime + self.packageSize / self.transmissionRate
         self.beingUsed = 0
 
+def nodeProcess(env, nd, ndCoordinator):
+    print(f"Node {nd.name} is trying to connect to coodinator at {env.now: .2f}.")
+    with ndCoordinator.manage.request() as req:
+        yield req
+        print(f"Node {nd.name} started transmission at {env.now: .2f}.")
+        yield env.process(ndCoordinator.answerToNode(nd.name, nd.packageSize/nd.transmissionRate))
+        nd.endTransmission()
+        print(f"Node {nd.name} finish transmission at {env.now: .2f}.")
 
-
-def setup(env):
-    ndMgt = NodesMgt(env, NUN_NODES)
+def setup(env, ndMgt):
     ndMgt.showAllNodes()
+    ndCoordinator = ndMgt.coordinator
     while True:
-        random_time = max(10, np.random.normal(100, 20))
+        random_time = max(2, np.random.normal(10, 5))
         yield env.timeout(random_time)
         nd = ndMgt.selectNode()
-        nd.defineTransmissionRate()
-        nd.generatePackageSize()
-        print(f"Node name {nd.name} is trying to connect to coodinator at {env.now: .2f}.")
-        ndCoordinator = ndMgt.coordinator
-        with ndCoordinator.manage.request() as req:
-            yield req
-            print(f"Node name {nd.name} started transmission.")
-            yield env.process(ndCoordinator.answerToNode(nd.name, nd.packageSize/nd.transmissionRate))
-            nd.endTransmission()
-            print(f"Node {nd.name} finish transmission")
-        
+        if nd is not None:
+            nd.defineTransmissionRate()
+            nd.generatePackageSize()
+            env.process(nodeProcess(env, nd, ndCoordinator))
 
 
-print("Starting Star Topology")
-
-env = simpy.Environment()
-env.process(setup(env))
-env.run(until = SIM_TIME)
-
-
-    
-# ndMgt.showAllNodes()
-
-# listOfNodesConnected = []
-
-# def node(env, name, coordinator):
-#     global listOfNodesConnected
-#     print(f"Node {name} trying to contact cordenator at {env.now: .2f}")
-#     with coordinator.manage.request() as request:
-#         listOfNodesConnected.append(name)
-#         yield request
-#         print(listOfNodesConnected)
-#         print(f"Node {name} connect to coordinator at {env.now: .2f}")
-#         yield env.process(coordinator.answerToNode(name))
-#         listOfNodesConnected.remove(name)
-#         print(f"Node {name} finish sent package to coordinator at {env.now: .2f}")
+if __name__ == '__main__':
+    print("Starting Star Topology")
+    env = simpy.Environment()
+    ndMgt = NodesMgt(env, NUN_NODES)
+    env.process(setup(env, ndMgt))
+    env.run(until = SIM_TIME)
+    ndMgt.showAllNodes()
